9c1a54f4c10c8ed6db8f5736dead2a5e462ad53b
srivatsan0611 <srivatsansrinivasan03@gmail.com>
2024-08-06 19:06:23 +0530

    Test API Methods - GET/POST/PUT/DELETE

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@
 // DB - y7Qd99tkzOiCRUGy
 //nodemon for any changes to sync
 
+//Patch for just updating few fields
+//Put for almost all?
+
 import express from "express"; // ES6 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -15,48 +18,34 @@ app.use(express.json()); // Middleware : parses all request.body to JSON
 
 
 app.get("/api/products", async (req,res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({success:true,data:products});
-
-    } catch (error) {
-        console.log(`Error in fetching products: ${error.message}`);
-        res.status(500).json({success:false,message:"Server Error"});
-    }
+    
 })
 
 app.post("/api/products", async (req,res) => {
-    const product = req.body; // User sends this data
+    
+});
 
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({success:false, message: "Please provide all fields"});
+app.put("/api/products/:id", async (req,res) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({success:false,message: "Product Not Found"});
     }
-
-    const newProduct = new Product(product);
 
     try {
-        await newProduct.save();
-        res.status(201).json({success:true, data: newProduct});
-    }
-    catch (error) {
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({success: false, message: "Server Error"});
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success:true, data: updatedProduct });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({success:false,message: "Server Error"});
+        
     }
 });
 
 //Deleting a product endpoint (URI)
 app.delete("/api/products/:id", async (req,res) => {
-    const {id} = req.params;
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success:true,message:"Successfully deleted"});
-        //console.log("id: ",id); 
-    }
-    catch (error) {
-        console.log(`Error in deleting product: ${error.message}`);
-        res.status(404).json({success:false,message:"Product you mentioned doesn't exist"});
-    }
+    
 })
 
 
