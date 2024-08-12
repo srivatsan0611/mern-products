d4ff357b6853b370e04926e3d6c362181515327d
srivatsan0611 <srivatsansrinivasan03@gmail.com>
2024-08-12 10:42:16 +0530

    Fix LightMode/DarkMode bug lol

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,16 @@
-import { Button, Container, Flex, HStack, Text, useColorModeValue } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom';
-import {PlusSquareIcon} from '@chakra-ui/icons';
-import { useColorMode } from '@chakra-ui/react';
+import { Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+import { PlusSquareIcon } from "@chakra-ui/icons";
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
 
 const Navbar = () => {
-  const { colorMode, toggleColorMode } = useColorMode()
+	const { colorMode, toggleColorMode } = useColorMode();
 
-  return (
-    <Container maxW="1140px" px={"4"}>
-        <Flex
+	return (
+		<Container maxW={"1140px"} px={4}>
+			<Flex
 				h={16}
 				alignItems={"center"}
 				justifyContent={"space-between"}
@@ -20,7 +19,7 @@ const Navbar = () => {
 					sm: "row",
 				}}
 			>
-            <Text
+				<Text
 					fontSize={{ base: "22", sm: "28" }}
 					fontWeight={"bold"}
 					textTransform={"uppercase"}
@@ -29,21 +28,20 @@ const Navbar = () => {
 					bgClip={"text"}
 				>
 					<Link to={"/"}>Product Store 🛒</Link>
-			</Text>
-            <HStack spacing={2} alignItems={"center"}>
-                <Link to={"/create"}>
-                <Button>
-                    <PlusSquareIcon fontSize={20}/>
-                </Button>
-                <Button onClick={toggleColorMode}>
-                    {colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
-                </Button>
-                </Link>
+				</Text>
 
-            </HStack>
-        </Flex>
-    </Container>
-  )
-}
-
+				<HStack spacing={2} alignItems={"center"}>
+					<Link to={"/create"}>
+						<Button>
+							<PlusSquareIcon fontSize={20} />
+						</Button>
+					</Link>
+					<Button onClick={toggleColorMode}>
+						{colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
+					</Button>
+				</HStack>
+			</Flex>
+		</Container>
+	);
+};
 export default Navbar;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,31 +1,31 @@
-import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react'
+import { Container, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useProductStore } from "../store/product";
-import { ProductCard } from "../components/ProductCard.jsx"
+import ProductCard from "../components/ProductCard";
 
 const HomePage = () => {
-  const { fetchProducts, products } = useProductStore();
+	const { fetchProducts, products } = useProductStore();
 
 	useEffect(() => {
 		fetchProducts();
 	}, [fetchProducts]);
 	console.log("products", products);
 
-  return (
-    <Container maxW={'container.x1'} py={12}>
-      <VStack spacing={8}>
-      <Text
+	return (
+		<Container maxW='container.xl' py={12}>
+			<VStack spacing={8}>
+				<Text
 					fontSize={"30"}
 					fontWeight={"bold"}
 					bgGradient={"linear(to-r, cyan.400, blue.500)"}
 					bgClip={"text"}
 					textAlign={"center"}
 				>
-					Current Products ️‍🔥️‍🔥️‍🔥
+					Current Products 🚀
 				</Text>
 
-        <SimpleGrid
+				<SimpleGrid
 					columns={{
 						base: 1,
 						md: 2,
@@ -34,12 +34,13 @@ const HomePage = () => {
 					spacing={10}
 					w={"full"}
 				>
-          {products.map((product) => (
-            <ProductCard key={product._id} product={product}/>
-          ))}
+					{products.map((product) => (
+						<ProductCard key={product._id} product={product} />
+					))}
 				</SimpleGrid>
-        
-        <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
+
+				{products.length === 0 && (
+					<Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
 						No products found 😢{" "}
 						<Link to={"/create"}>
 							<Text as='span' color='blue.500' _hover={{ textDecoration: "underline" }}>
@@ -47,12 +48,9 @@ const HomePage = () => {
 							</Text>
 						</Link>
 					</Text>
-       
-
-
-      </VStack>
-    </Container>
-  )
-}
-
-export default HomePage
+				)}
+			</VStack>
+		</Container>
+	);
+};
+export default HomePage;
